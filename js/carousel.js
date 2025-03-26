1cd010d9e56339fc06efb12aa64a5cfe4daa7dfd
fff907 <59164817+fff907@users.noreply.github.com>
2025-03-26 16:24:09 +0900

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 		autoplaySpeed: 5000, // 自動再生の速さ、単位はミリ秒
 		arrows: false, // 左右のナビゲーションを無効にする
 		dots: true, // スライド下にドットを表示する
-		fade: true, // スライドの切り替えをフェードにする        
+		fade: true, // スライドの切り替えをフェードにする
+		cssEase: 'linear'
 	});
 });
