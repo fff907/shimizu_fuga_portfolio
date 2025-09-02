b61543602bb1e205e0d367739370d0acda8f855d
fff907 <59164817+fff907@users.noreply.github.com>
2025-09-02 17:18:24 +0900

    Update hamburger.js

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,10 +1,22 @@
-$(document).ready(function() {
-	// ハンバーガーメニューボタンのクリックイベント
-	$(".header__hamburger").click(function(event) {
-	$(".nav--sp").slideToggle();
-	});
-	// ナビゲーションリンクのクリックイベント
-	$(".nav__list--sp a").click(function(event) {
-	$(".nav--sp").slideUp();
-	});
-	});
+'use strict';
+
+{
+  const hamburger = document.querySelector('.header__hamburger');
+  const nav = document.querySelector('.nav--sp');
+  const navLinks = document.querySelectorAll('.nav__list--sp a');
+
+  // ハンバーガーボタンをクリックしたとき、メニューを開閉
+  hamburger.addEventListener('click', () => {
+    nav.classList.toggle('is-open'); // メニュー表示,非表示を切り替える
+    hamburger.classList.toggle('is-active'); // ボタンの状態も切り替え
+  });
+
+  // ナビ内のリンクをクリックしたらメニューを閉じる
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      nav.classList.remove('is-open');
+      hamburger.classList.remove('is-active');
+    });
+  });
+}
+
