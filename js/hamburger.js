3619b7d2070009f16fd867940b5a8bb6b20a7fdc
fff907 <59164817+fff907@users.noreply.github.com>
2025-09-03 11:01:40 +0900

    Update hamburger.js

--- js/hamburger.js
+++ js/hamburger.js
@@ -3,19 +3,12 @@
 {
   const open = document.getElementById('open');         // ハンバーガー（三本線）
   const overlay = document.querySelector('.overlay');   // オーバーレイ背景＋メニュー
-  const close = document.getElementById('close');        // ×ボタン
   const navLinks = document.querySelectorAll('.nav__list--sp a'); // 各ナビリンク
 
   // ハンバーガーをクリックするとオーバーレイを表示
   open.addEventListener('click', () => {
     overlay.classList.add('show');
     open.classList.add('is-active');
-  });
-
-  // ×ボタンでオーバーレイを閉じる
-  close.addEventListener('click', () => {
-    overlay.classList.remove('show');
-    open.classList.remove('is-active');
   });
 
   // 各リンククリックでもオーバーレイを閉じる
