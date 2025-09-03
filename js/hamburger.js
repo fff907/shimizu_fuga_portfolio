7161f0af5a06ea873ac06e7698d87296bdc2ceff
fff907 <59164817+fff907@users.noreply.github.com>
2025-09-03 11:13:24 +0900

    Update hamburger.js

--- js/hamburger.js
+++ js/hamburger.js
@@ -4,18 +4,25 @@
   const open = document.getElementById('open');
   const overlay = document.querySelector('.overlay');
   const navLinks = document.querySelectorAll('.nav__list--sp a');
-  
-  // ハンバーガーをクリックするとオーバーレイを開閉
+
   open.addEventListener('click', () => {
-    overlay.classList.toggle('show');
+    const isOpen = overlay.classList.toggle('show');
     open.classList.toggle('is-active');
+
+    // スクロール禁止・解除
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
   });
 
-  // 各リンククリック時はメニューを閉じる
+  // 各リンククリックでもメニューを閉じてスクロール再開
   navLinks.forEach(link => {
     link.addEventListener('click', () => {
       overlay.classList.remove('show');
       open.classList.remove('is-active');
+      document.body.style.overflow = '';
     });
   });
 }
